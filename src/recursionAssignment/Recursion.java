52e7d1d69258f4156996335b9608b4187189d8dd
alexmeng03 <alex.meng21@ycdsbk12.ca>
2020-12-05 18:18:57 +0000

    added recursive cases test1 green

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -9,7 +9,11 @@ public class Recursion{
    // base case
    if (n == 0){
      return 0;
+     // recursive case
+   }else if (n % 10 == 7){
+     return 1 + count7(n / 10);
    }
+    return count7(n / 10);
  }
 
  public static String pairStar(String str){
